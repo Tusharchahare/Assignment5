8341744e94bc98c52733997ec1be3f530512bb35
Tusharchahare <tusharchahare2001@gmail.com>
2026-01-31 16:35:07 +0530

    added  AddTwoNum.java

---
+++ AddTwoNum.java
@@ -0,0 +1,25 @@
+2) Write a program to add two numbers. 
+
+import java.util.Scanner;
+public class AddTwoNum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	Scanner sc = new Scanner(System.in);
+	
+	System.out.println("Enter two  number :");
+	
+			
+	int a = sc.nextInt();
+	
+	int b = sc.nextInt();	 
+	
+	
+	System.out.println("addition is :"+(a+b));
+			
+	}
+	
+
+}
+
