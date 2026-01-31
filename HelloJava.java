bb5f6eb5a771ba9e87c21af587f0314b8ff7df8f
Tusharchahare <tusharchahare2001@gmail.com>
2026-01-31 16:32:28 +0530

    added HelloJava.java program

---
+++ HelloJava.java
@@ -0,0 +1,12 @@
+1 Write a program to print "Hello, Java!" 
+
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("HELLO JAVA");
+		
+	}
+}
